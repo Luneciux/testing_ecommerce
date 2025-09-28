7cf7b517c6f4e7b16c63f0e568528f6bf3ec0c4b
Eliel Costa <jejosu346@gmail.com>
2025-09-28 16:45:18 -0300

    test: adding utils functions for cart and products

--- cypress/utils/utils.js
+++ cypress/utils/utils.js
@@ -5,7 +5,27 @@ const Utils = {
     let alertMessage = '';
     cy.on('window:alert', (str) => alertMessage = str);
     return cy.wrap(null).then(() => alertMessage);
-  }
+  },
+
+  getCartTotal: () => cy.get('#cart-total'),
+
+  mockResponseWithFixture: (url, fixture, alias) => {
+
+    let parseIndex = fixture.split('.');
+    cy.fixture(parseIndex[0]).then((data) => {
+
+      let body = parseIndex.length
+       ? parseIndex.reduce((obj, key) => obj[key], data)
+       : data;
+
+      cy.intercept(url, { body: body }).as(alias);
+      
+    });
+  },
+
+  typeOnInputBySelector: (selector, text) => {
+    cy.get(selector).type(text);
+  },
 
 }
 
