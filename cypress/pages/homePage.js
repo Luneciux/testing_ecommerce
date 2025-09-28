b63607ea37e79047dabcd4f19c7c6a3d99f89461
Eliel Costa <jejosu346@gmail.com>
2025-09-28 16:43:35 -0300

    test: adding products elements, adding get and type product functions

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -5,6 +5,11 @@ const elements = {
   logoutButton: () => cy.get('#logout-btn'),
   userNameSpan: () => cy.get('#user-name'),
   textLogoH1: () => cy.get('header h1').contains('BIX Mini E-commerce'),
+  productsSection: () => cy.get('section:has(#product-list)'),
+  productsSectionTitle: () => cy.get('section').contains('Produtos'),
+  productsList: () => cy.get('#product-list'),
+  productsListItems: () => cy.get('#product-list').find('li'),
+  mainSection: () => cy.get('main'),
 }
 
 const HomePage = { 
@@ -13,6 +18,16 @@ const HomePage = {
   fillPassword: (password) => elements.passwordInput().type(password),
   clickLogin: () => elements.loginButton().click(),
   clickLogout: () => elements.logoutButton().click(),
+  getProductActionsByIndex: (productIndex) => elements.productsListItems().eq(productIndex).find('button'),
+  addProductByIndex: (productIndex) => HomePage.getProductActionsByIndex(productIndex).contains('Adicionar').click(),
+  typeByIndexProductQuantity: (productIndex, quantity) => {
+    elements.productsListItems()
+      .eq(productIndex)
+      .get(`#qty-${productIndex + 1}`)
+      .clear()
+      .type(quantity);
+  },
+  getProductQuantityByIndex: (productIndex) => elements.productsListItems().eq(productIndex).get(`#qty-${productIndex + 1}`),
 }
 
 export default HomePage;
